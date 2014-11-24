cae6be331142cd57233e8df52dfddd3533173164
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-11-24 14:52:27 -0800

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :username, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+
 end
