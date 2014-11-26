3e82feb83927b269ff83a7d1724191938f923266
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-11-26 09:46:08 -0800

    add most recent wagers and predictions to users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
   def user_params
     params.require(:user).permit(:username, :password)
   end
