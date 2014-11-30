9a4929051193cfde2d7b10e0dc78b6e37e288d8e
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-11-29 19:26:09 -0800

    create javascript boilerplate

---
+++ app/assets/javascripts/prediction_note.js
@@ -0,0 +1,11 @@
+window.PredictionNote = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  Utils: {},
+  initialize: function() {
+    // new PredictionNote.Routers.Router
+    // Backbone.history.start();
+  }
+};
