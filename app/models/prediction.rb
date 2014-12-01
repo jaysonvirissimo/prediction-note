a8449ebbf101ba6d2e4544e6448e25de14277ff8
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-12-01 13:54:19 -0800

    add judgment symbols to index

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -15,7 +15,7 @@ class Prediction < ActiveRecord::Base
   validates :statement, :deadline, presence: true
   belongs_to(:user)
   has_many(:wagers)
-  has_one(:judgment)
+  has_many(:judgments)
 
   def self.parse_datetime(string)
     Chronic.parse(string)
