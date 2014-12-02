6687ab100e48cfb1a719c1364e89a33b073b74a3
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-12-02 13:38:39 -0800

    add quotes to seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,60 @@ def create_judgments(number)
   end
 end
 
+def create_quotes
+  Quote.create!(
+    author: "Alex Tabarrok",
+    quote: "A bet is a tax on bullshit."
+  )
+
+  Quote.create!(
+  author: "Penn Jellete",
+  quote: "Luck is statistics taken personally."
+  )
+
+  Quote.create!(
+  author: "John Calvin",
+  quote: "I've got to start listening to those quiet, nagging doubts."
+  )
+
+  Quote.create!(
+  author: "Ken Wilber",
+  quote: "Nobody is smart enough to be wrong all the time."
+  )
+
+  Quote.create!(
+  author: "Voltaire",
+  quote: "Doubt is not a pleasant condition, but certainty is absurd."
+  )
+
+  Quote.create!(
+  author: "Warren E. Buffett",
+  quote: "The Noah principle: predicting rain doesn’t count, building arks does."
+  )
+
+  Quote.create!(
+  author: "George Bernard Shaw",
+  quote: "It is the mark of a truly intelligent person to be moved by statistics."
+  )
+
+  Quote.create!(
+  author: "Fred Mosteller",
+  quote: "It’s easy to lie with statistics, but it’s easier to lie without them."
+  )
+
+  Quote.create!(
+  author: "Hermann von Helmholtz",
+  quote: "The most practical thing in the world is a good theory."
+  )
+
+  Quote.create!(
+  author: "John Maynard Keynes",
+  quote: "When the facts change, I change my mind. What do you do, sir?"
+  )
+end
+
 create_users(16)
 create_predictions(64)
 create_wagers(128)
 create_judgments(64)
+create_quotes
