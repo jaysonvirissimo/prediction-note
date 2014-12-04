f0b2cf1a004351ae20d54012265f2a7e8b7cec2f
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-12-03 16:44:06 -0800

    create custom validation to prevent the creation of deadlines in the past

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -15,6 +15,7 @@ class Prediction < ActiveRecord::Base
   validates :statement, :deadline, presence: true
   validates :statement, length: { in: (1..140) }
   validates :statement, uniqueness: true
+  validate :date_must_be_in_future
   belongs_to(:user)
   has_many(:wagers)
   has_many(:judgments)
@@ -39,5 +40,15 @@ class Prediction < ActiveRecord::Base
     end
 
     (sum.to_f / wagers.length).round
+  end
+
+  private
+
+  def date_must_be_in_future
+    return if self.deadline.nil?
+
+    if self.deadline < Time.now
+      errors.add(:deadline, "must be in the future... this isn't RetrodictionNote")
+    end
   end
 end
