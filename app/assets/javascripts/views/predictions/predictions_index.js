e106b2a53fbcd142fa04de623b85a8966dd5ffa8
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-12-04 10:22:32 -0800

    add render function to predictions index backbone view

--- app/assets/javascripts/views/predictions/predictions_index.js
+++ app/assets/javascripts/views/predictions/predictions_index.js
@@ -1,5 +1,13 @@
 PredictionNote.Views.PredictionsIndex = Backbone.View.extend({
 
-  template: JST['predictions/index']
+  template: JST['predictions/index'],
+
+  render: function () {
+    var renderedContent = this.template({
+      predictions: this.collection
+    });
+    this.$el.html(renderedContent);
+    return this;
+  }
 
 });
